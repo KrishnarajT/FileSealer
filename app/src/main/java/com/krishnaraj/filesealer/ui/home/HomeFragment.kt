7248f49a4a0b8021cdd5fe8a16c7dcce3d6b46ea
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-10-06 22:29:01 +0530

    added code to prompt file opener

--- app/src/main/java/com/krishnaraj/filesealer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/krishnaraj/filesealer/ui/home/HomeFragment.kt
@@ -8,6 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.krishnaraj.filesealer.databinding.FragmentHomeBinding
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.widget.Button
+import com.krishnaraj.filesealer.R
+
 
 class HomeFragment : Fragment() {
 
@@ -28,6 +34,13 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // get the open file button.
+        val openFileButton: Button = root.findViewById(R.id.open_file_btn)
+        // attach an OnClickListener
+        openFileButton.setOnClickListener {
+            openFileExplorer()
+        }
+
         return root
     }
 
@@ -35,4 +48,26 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    private val PICK_FILE_REQUEST_CODE = 1
+
+    private fun openFileExplorer() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*" // Allow all file types
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_FILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            val selectedFileUri = data?.data
+            // Now you have the URI of the selected file, you can proceed with encryption.
+            // Example: val filePath = selectedFileUri?.path
+        }
+    }
+
+
 }
